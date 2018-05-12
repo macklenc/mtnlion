9702395c6505f6d361e96cdaa2ca91e2c214af77
Christopher Macklen <cmacklen@uccs.edu>
2018-05-11 23:41:36 -0400

    Fixed the developer 'getting started' process

--- tests/buildup/j.py
+++ tests/buildup/j.py
@@ -3,8 +3,8 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-import comsol
-import engine
+import mtnlion.comsol as comsol
+import mtnlion.engine as engine
 
 
 def nice_abs(number):
