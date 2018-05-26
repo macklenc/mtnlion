e4213710a05ee32b3bac017d1d6cf92b4b587824
Christopher Macklen <cmacklen@uccs.edu>
2018-05-25 22:23:20 -0400

    Separated comsol main into tools

--- mtnlion/comsol.py
+++ mtnlion/comsol.py
@@ -13,10 +13,8 @@ might have a repeated x=1, but only one x=2. In order to solve this, this module
 """
 import logging
 import os
-import sys
 from typing import List, Union, Dict
 
-import click
 import numpy as np
 
 from . import domain
@@ -181,60 +179,3 @@ def load(filename: str) -> domain.ReferenceCell:
     """
     file_data = loader.load_numpy_file(filename)
     return domain.ReferenceCell.from_dict(file_data)
-
-
-@click.command()
-@click.option('--dt', '-t', nargs=3, type=float, help='[start time stop time delta time]')
-@click.option('--bound', '-b', type=click.Path(exists=True, readable=True, resolve_path=True),
-              help='file containing boundaries, defaults to [1, 2]')
-@click.option('--critical', 'loglevel', flag_value=logging.CRITICAL, help='Set log-level to CRITICAL')
-@click.option('--error', 'loglevel', flag_value=logging.ERROR, help='Set log-level to ERROR')
-@click.option('--warn', 'loglevel', flag_value=logging.WARNING, help='Set log-level to WARNING')
-@click.option('--info', 'loglevel', flag_value=logging.INFO, help='Set log-level to INFO', default=True)
-@click.option('--debug', 'loglevel', flag_value=logging.DEBUG, help='Set log-level to DEBUG')
-@click.argument('output', type=click.Path(writable=True, resolve_path=True))
-@click.argument('input_files', nargs=-1, type=click.Path(exists=True, readable=True, resolve_path=True))
-def main(input_files: List[str], output: Union[click.utils.LazyFile, str],
-         dt: List[float], loglevel: Union[None, int], bound: Union[None, str]) -> Union[None, int]:
-    """
-    Convert COMSOL CSV files to npz.
-
-    Create a numpy zip (npz) with variables corresponding to the csv file names.
-    Each variable contains the data from the file as a list. Additionally, each
-    variable is a key in the main dictionary.
-    """
-
-    logging.basicConfig(level=loglevel)
-
-    if not input_files:
-        logger.error('No CSVs were specified. Aborting.')
-        sys.exit(1)
-
-    if not bound:
-        bound = [1, 2]
-    else:
-        bound = loader.load_csv_file(bound)
-
-    logger.info('Output file: {}'.format(output))
-    logger.info('Input file(s): {}'.format(input_files))
-    logger.info('dt: {}'.format(dt))
-    logger.info('boundaries: {}'.format(bound))
-
-    file_data = loader.collect_files(input_files, format_key=format_name, loader=loader.load_csv_file)
-    if 'time_mesh' not in file_data:
-        try:
-            file_data['time_mesh'] = np.arange(dt[0], dt[1] + dt[2], dt[2])
-        except IndexError as ex:
-            logger.critical('Either a dt option provided with start and stop time (inclusive) or a csv defining the '
-                            'time mesh is required', exc_info=True)
-            raise ex
-
-    data = format_data(file_data, boundaries=bound)
-    loader.save_npz_file(output, data)
-
-    logger.info('Conversion completed successfully')
-    return 0
-
-
-if __name__ == '__main__':
-    sys.exit(main())
