3990853c96257e7e0efbbd3392b4b5b4be6ad160
Christopher Macklen <cmacklen@uccs.edu>
2018-05-26 13:09:01 -0400

    Updated COMSOL parsing to match new export format and replaced data set
    Resolves #9. Turns out the data set being used was not based off the
    same parameters as the parameters in the spreadsheet. This commit
    updates the dataset and parsing algorithms. Also updated phis.py to
    be more verbose.

--- mtnlion/comsol.py
+++ mtnlion/comsol.py
@@ -1,15 +1,11 @@
 """
 COMSOL Data Handling. This module is designed to load 1D solution data from a Gu & Wang reference model from COMSOL as
 CSV files. The idea is that CSV files take a long time to load, so it is more efficient to convert the data to a binary
-(npz) format before processing. When converting, this module will also reformat the data from COMSOL's rather confusing
-output format.
+(npz) format before processing.
 
-My current hypothesis is that the discontinuities at the internal boundaries from the solutions are due to the boundary
-being computed for both the left and right side. I.e. at x=1 on the mesh, there could be two solutions, y=1 and y=NaN.
-It also appears that COMSOL for some strange reason flips the order on the boundaries, such that the first x=1 position
-is for the right side of the boundary, while the second is the left side. Thus, this module will reverse these. Another
-odd formatting choice from COMSOL is to conditionally add the repeated x coordinate, i.e. the first time sample of j
-might have a repeated x=1, but only one x=2. In order to solve this, this module will duplicate non-repeated boundaries.
+COMSOL now saves it's data as a 2D matrix, however it still only uses repeated x values when the boundary solves for
+different values on either side. In order to normalize the repeated bounds, all bounds are check to ensure they've got
+repeated x values, such that the y values are duplicated.
 """
 import logging
 import os
@@ -27,12 +23,8 @@ def fix_boundaries(mesh: np.ndarray, data: np.ndarray, boundaries: Union[float, 
         -> Union[None, np.ndarray]:
     """
     When COMSOL outputs data from the reference model there are two solutions at every internal boundary, which causes
-    COMSOL to have repeated domain values; one for the right and one for the left of the boundary. For some asinine
-    reason, when the solution on both sides of the boundary is the same, they decided to save space and remove the
-    repeated value, resulting in a mesh that fluctuates in length over time. Additionally, for some god-awful reason
-    the left and right boundaries are switched in the mesh, i.e. the left boundary comes after the right boundary. This
-    method is intended to fix this... If there is only one internal boundary on the variable mesh at a given time,
-    then a duplicate is added, otherwise it will switch the two values.
+    COMSOL to have repeated domain values; one for the right and one for the left of the boundary. If there is only one
+    internal boundary on the variable mesh at a given time, then a duplicate is added.
 
     :param mesh: x data to use to correct the y data
     :param data: in 2D, this would be the y data
@@ -46,13 +38,9 @@ def fix_boundaries(mesh: np.ndarray, data: np.ndarray, boundaries: Union[float, 
         return data
 
     for x in b_indices[::-1]:
-        if mesh[x] == mesh[x + 1]:  # swap
-            logger.debug('Swapping boundaries {x0}, {x1}, at indices {i0}, {i1}'.format(
-                x0=mesh[x], x1=mesh[x + 1], i0=x, i1=x + 1))
-            (data[x], data[x + 1]) = (data[x + 1], data[x])
-        else:  # add boundary
+        if mesh[x] != mesh[x + 1]:  # add boundary
             logger.debug('Adding boundary, copying {x} at index {i}'.format(x=mesh[x], i=x))
-            data = np.insert(data, x, data[x])
+            data = np.insert(data, x, data[x], axis=0)
 
     return data
 
@@ -85,30 +73,6 @@ def get_standardized(cell: domain.ReferenceCell) -> Union[domain.ReferenceCell, 
     # return domain.ReferenceCell(mesh, cell.time_mesh, cell.boundaries, **new_data)
 
 
-def separate_frames(mesh: np.ndarray, data: np.ndarray, boundaries: List[int]) -> np.ndarray:
-    """
-    COMSOL saves its data as two columns, spaces and data, which is repeated (in the same column) for every time step.
-    This breaks out the 1D data in to 2D to be able to easily reference time and space. The boundaries are fixed
-    so that the data has uniform length and can fit in a matrix.
-
-    :param mesh: mesh corresponding to the data
-    :param data: data that maps to the mesh
-    :param boundaries: internal boundaries
-    :return: 2D normalized data in time and space
-    """
-    logger.info('Separating data time segments')
-    # Separate time segments (frames)
-    time_frame = np.nonzero(np.diff(mesh) < 0)[0]
-    # Frame start indices
-    start = np.insert(time_frame + 1, 0, 0)
-    # Frame stop indices
-    stop = np.append(time_frame, len(mesh) - 1) + 1
-    logger.info('Found {time} time steps'.format(time=len(start)))
-    # collect y_data determining if there are discontinuous boundaries
-    return np.array([fix_boundaries(mesh[start[i]:stop[i]], data[start[i]:stop[i]], boundaries)
-                     for i in range(len(start))])
-
-
 def format_data(raw_data: Dict[str, np.ndarray], boundaries: Union[float, List[int]])\
         -> Union[Dict[str, np.ndarray], None]:
     """
@@ -135,8 +99,9 @@ def format_data(raw_data: Dict[str, np.ndarray], boundaries: Union[float, List[i
 
         logger.info('Reformatting {}'.format(key))
         try:
-            (x_data, y_data) = (value[:, 0], value[:, 1])
-            data[key] = separate_frames(x_data, y_data, boundaries)
+            (x_data, y_data) = (value[:, 0], value[:, 1:])
+
+            data[key] = fix_boundaries(x_data, y_data, boundaries).T
 
             if data[key].shape[-1] != len(raw_data['mesh']):
                 logger.warning('{} does not fit the mesh, skipping'.format(key))
