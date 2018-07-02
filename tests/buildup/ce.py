099d5d14c5b9e3a0a3f44809988d38b8ef5a0448
Christopher Macklen <cmacklen@uccs.edu>
2018-07-02 16:48:23 -0400

    Some more buildup refactoring

--- tests/buildup/ce.py
+++ tests/buildup/ce.py
@@ -2,8 +2,7 @@ import fenics as fem
 import matplotlib.pyplot as plt
 import numpy as np
 
-import domain2
-import mtnlion.comsol as comsol
+import common
 import mtnlion.engine as engine
 import utilities
 
@@ -17,29 +16,24 @@ def ce():
     time[::2] = [t-dt for t in time_in]
     time[1::2] = time_in
 
-    # Collect required data
-    # TODO: make sure refactored comsol works here
-    comsol_data, params = utilities.gather_data()
-    time_ind = engine.find_ind_near(comsol_data.time_mesh, time)
-    data = comsol.get_standardized(comsol_data.filter_time(time_ind))
+    # Collect common data
+    cmn = common.Common(time)
 
     # initialize matrix to save solution results
-    u_array = np.empty((len(time_in), len(data.mesh)))
-    mesh, dx, ds, bm, dm = domain2.generate_domain(data.mesh)
+    u_array = np.empty((len(time_in), len(cmn.comsol_solution.mesh)))
 
-    # Initialize parameters
-    eps_e = utilities.mkparam(dm, params.neg.eps_e, params.sep.eps_e, params.pos.eps_e)
-    de_eff = utilities.mkparam(dm, params.const.De_ref * params.neg.eps_e ** params.neg.brug_De,
-                     params.const.De_ref * params.sep.eps_e ** params.sep.brug_De,
-                     params.const.De_ref * params.pos.eps_e ** params.pos.brug_De)
-    t_plus = fem.Constant(params.const.t_plus)
-    Lc = utilities.mkparam(dm, params.neg.L, params.sep.L, params.pos.L)
-    a_s = utilities.mkparam(dm, 3 * params.neg.eps_s / params.neg.Rs, 0, 3 * params.pos.eps_s / params.pos.Rs)
+    # create local variables
+    comsol_sol = cmn.comsol_solution
+    mesh, dx, ds, bm, dm = cmn.mesh, cmn.dx, cmn.ds, cmn.bm, cmn.dm
 
-    # plt.plot(data.mesh, data.data.ce[1])
-    # plt.show()
+    eps_e = cmn.eps_e
+    de_eff = cmn.de_eff
+    t_plus = cmn.t_plus
+    Lc = cmn.Lc
+    a_s = cmn.a_s
+
     k = 0
-    for i, j in enumerate(data.data.j[1::2]):
+    for i, j in enumerate(comsol_sol.data.j[1::2]):
         i_1 = i * 2
         i = i*2 + 1
 
@@ -49,15 +43,13 @@ def ce():
         v = fem.TestFunction(V)
 
         # Initialize Dirichlet BCs
-        bc = [fem.DirichletBC(V, data.data.ce[i, 0], bm, 1), fem.DirichletBC(V, data.data.ce[i, -1], bm, 4)]
+        bc = [fem.DirichletBC(V, comsol_sol.data.ce[i, 0], bm, 1), fem.DirichletBC(V, comsol_sol.data.ce[i, -1], bm, 4)]
         jbar = fem.Function(V)
-        data.data.j[i_1, 21] = data.data.j[i_1, 20]
-        data.data.j[i_1, 49] = data.data.j[i_1, 50]
-        jbar.vector()[:] = data.data.j[i_1, fem.dof_to_vertex_map(V)].astype('double')
+        jbar.vector()[:] = comsol_sol.data.j[i_1, fem.dof_to_vertex_map(V)].astype('double')
         # fem.plot(jbar)
         # plt.show()
         ce_1 = fem.Function(V)
-        ce_1.vector()[:] = data.data.ce[i_1, fem.dof_to_vertex_map(V)].astype('double')
+        ce_1.vector()[:] = comsol_sol.data.ce[i_1, fem.dof_to_vertex_map(V)].astype('double')
 
         boundary_markers = fem.MeshFunction('size_t', mesh, mesh.topology().dim() - 1)
         boundary_markers.set_all(0)
@@ -95,13 +87,13 @@ def ce():
     # plt.grid()
     # plt.show()
 
-    print(engine.rmse(u_array, data.data.ce[1::2]))
+    print(engine.rmse(u_array, comsol_sol.data.ce[1::2]))
 
     coor = mesh.coordinates()
     # for i in range(len(u_array)):
     #     print('u(%8g) = %g' % (coor[i], u_array[len(u_array)-1-i]))
 
-    utilities.overlay_plt(data.mesh, time_in, '$c_e$', u_array, data.data.ce[1::2])
+    utilities.overlay_plt(comsol_sol.mesh, time_in, '$c_e$', u_array, comsol_sol.data.ce[1::2])
 
     plt.savefig('comsol_compare_ce.png')
     plt.show()
