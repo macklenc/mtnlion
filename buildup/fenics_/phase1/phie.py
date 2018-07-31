3c05a874ca99831348a295239724f49cc27034da
Christopher Macklen <cmacklen@uccs.edu>
2018-07-31 15:13:00 -0400

    Added TODO's for Neumann conditions

--- buildup/fenics_/phase1/phie.py
+++ buildup/fenics_/phase1/phie.py
@@ -15,6 +15,7 @@ def run(time, return_comsol=False):
     jbar_c, ce_c, phie = utilities.create_functions(domain.V, 3)
     kappa_eff, kappa_Deff = common.kappa_Deff(ce_c, **cmn.params, **cmn.const)
 
+    # TODO: add internal neumann conditions
     a, L = equations.phie(jbar_c, ce_c, phie_u, v, domain.dx, kappa_eff, kappa_Deff,
                           **cmn.params, **cmn.const, nonlin=False)
 
