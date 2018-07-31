fda7c3f71cc75a1136bf83543ea2c6edc8711a8d
Christopher Macklen <cmacklen@uccs.edu>
2018-07-31 15:09:43 -0400

    Added run function to phase1.phis

--- buildup/fenics_/phase1/phis.py
+++ buildup/fenics_/phase1/phis.py
@@ -5,9 +5,7 @@ from buildup import (common, utilities)
 from mtnlion.newman import equations
 
 
-def main():
-    # Times at which to run solver
-    time = [0, 5, 10, 15, 20]
+def run(time, return_comsol=False):
     cmn, domain, comsol = common.prepare_comsol_buildup(time)
 
     phis_sol = utilities.create_solution_matrices(len(time), len(comsol.mesh), 1)[0]
@@ -19,7 +17,7 @@ def main():
     Iapp = fem.Constant(0)
 
     a, L = equations.phis(jbar_c, phis_u, v, domain.dx((0, 2)), **cmn.params, **cmn.const,
-                          neumann=Iapp/cmn.const.Acell, ds=domain.ds(4), nonlin=False)
+                          neumann=Iapp / cmn.const.Acell, ds=domain.ds(4), nonlin=False)
     a += fem.dot(phis_u, v) * domain.dx(1)
     # same as
     # a2, L2 = equations.phis(fem.Constant(0), phis_u, v, domain.dx(1), **cmn.params, **cmn.const, nonlin=False)
@@ -31,10 +29,20 @@ def main():
         Iapp.assign(cmn.Iapp[i])
         bc[1] = fem.DirichletBC(domain.V, comsol.data.phis[i, -1], domain.boundary_markers, 4)
 
-        # Solve
         fem.solve(a == L, phis, bc)
         phis_sol[i, :] = utilities.get_1d(phis, domain.V)
 
+    if return_comsol:
+        return phis_sol, comsol
+    else:
+        return phis_sol
+
+
+def main():
+    # Times at which to run solver
+    time = [0, 5, 10, 15, 20]
+
+    phis_sol, comsol = run(time, return_comsol=True)
     utilities.report(comsol.neg, time, phis_sol[:, comsol.neg_ind],
                      comsol.data.phis[:, comsol.neg_ind], '$\Phi_s^{neg}$')
     plt.savefig('comsol_compare_phis_neg.png')
