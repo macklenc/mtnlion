89195d39d9fa3da85a021effd61e0b7c4a9e7bcf
Christopher Macklen <cmacklen@uccs.edu>
2018-08-01 10:49:24 -0400

    Added phis and phie for phase2
    * phie's solution is currently divergent

--- buildup/fenics_/phase2/phis.py
+++ buildup/fenics_/phase2/phis.py
@@ -1,61 +1,70 @@
-from functools import partial
-
 import fenics as fem
 import matplotlib.pyplot as plt
-import numpy as np
 
-import common
-import utilities
+from buildup import (common, utilities)
 from mtnlion.newman import equations
 
 
-def solve(time, domain, Acell, sigma_eff, L, a_s, F, Iapp, true_sol):
-    # initialize matrix to save solution results
-    u_array = np.empty((len(time), len(true_sol.mesh)))
+def run(time, solver, return_comsol=False):
+    cmn, domain, comsol = common.prepare_comsol_buildup(time)
+
+    phis_sol = utilities.create_solution_matrices(int(len(time) / 2), len(comsol.mesh), 1)[0]
+    bc = [fem.DirichletBC(domain.V, 0.0, domain.boundary_markers, 1), 0]
     phis_u = fem.TrialFunction(domain.V)
     v = fem.TestFunction(domain.V)
-    dx = (domain.dx(0) + domain.dx(2))
-    # phi_s = Phis(Acell, sigma_eff, L, a_s, F, phis, v, dx, domain.ds(4))
 
-    bm = domain.boundary_markers
-    V = domain.V
+    phis_c_, phie_c, ce_c, cse_c = utilities.create_functions(domain.V, 4)
+    phis = utilities.create_functions(domain.V, 1)[0]
+    Iapp = fem.Constant(0)
 
-    jbar = fem.Function(domain.V)
-    phis = fem.Function(domain.V)
+    # TODO: Fix j
+    # j = equations.j(ce_c, cse_c, phie_c, phis_c_, **cmn.params, **cmn.const)
+    j = equations.j_new(ce_c, cse_c, phie_c, phis_c_, **cmn.params, **cmn.const, dm=domain.domain_markers, V=domain.V)
 
-    phis_eq = partial(equations.phis, jbar, a_s, F, sigma_eff, L, phis_u, v, domain.dx, domain.ds(4), nonlin=False)
+    a, L = equations.phis(j, phis_u, v, domain.dx((0, 2)), **cmn.params, **cmn.const,
+                          neumann=Iapp / cmn.const.Acell, ds=domain.ds(4), nonlin=False)
+    a += fem.dot(phis_u, v) * domain.dx(1)
 
-    bc = [fem.DirichletBC(V, 0.0, bm, 1), 0]
-    for i, j in enumerate(true_sol.data.j):
-        jbar.vector()[:] = j[fem.dof_to_vertex_map(V)].astype('double')
-        bc[1] = fem.DirichletBC(V, true_sol.data.phis[i, -1], bm, 4)
+    k = 0
+    for i in range(int(len(time) / 2)):
+        i_1 = i * 2  # previous time step
+        i = i * 2 + 1  # current time step
+        utilities.assign_functions([comsol.data.phis], [phis_c_], domain.V, i_1)
+        utilities.assign_functions([comsol.data.phie, comsol.data.ce, comsol.data.cse],
+                                   [phie_c, ce_c, cse_c], domain.V, i)
+        Iapp.assign(cmn.Iapp[i])
+        bc[1] = fem.DirichletBC(domain.V, comsol.data.phis[i, -1], domain.boundary_markers, 4)
 
-        a, Lin = phis_eq(neumann=fem.Constant(Iapp[i]) / Acell)
+        solver(a == L, phis, phis_c_, bc)
+        phis_sol[k, :] = utilities.get_1d(phis, domain.V)
+        k += 1
 
-        # Solve
-        fem.solve(a == Lin, phis, bc)
-        u_array[i, :] = phis.vector().get_local()[fem.vertex_to_dof_map(domain.V)]
-
-    return u_array, true_sol
+    if return_comsol:
+        return phis_sol, comsol
+    else:
+        return phis_sol
 
 
 def main():
+    # Quiet
+    fem.set_log_level(fem.ERROR)
+
     # Times at which to run solver
-    time = [0, 5, 10, 15, 20]
-    I_1C = 20.5
-    Iapp = [I_1C if 10 <= i <= 20 else -I_1C if 30 <= i <= 40 else 0 for i in time]
+    time_in = [0.1, 5, 10, 15, 20]
+    # time_in = np.arange(0.1, 50, 0.1)
+    dt = 0.1
+    time = [None] * (len(time_in) * 2)
+    time[::2] = [t - dt for t in time_in]
+    time[1::2] = time_in
 
-    cmn = common.Common(time)
-    domain = cmn.domain
-    L, a_s, sigma_eff = common.collect(cmn.params, 'L', 'a_s', 'sigma_eff')
-    F, Acell = common.collect(cmn.const, 'F', 'Acell')
-
-    fenics, comsol = solve(time, domain, Acell, sigma_eff, L, a_s, F, Iapp, cmn.comsol_solution)
-    utilities.report(comsol.neg, time, fenics[:, comsol.neg_ind], comsol.data.phis[:, comsol.neg_ind], '$\Phi_s^{neg}$')
-    plt.savefig('comsol_compare_phis_neg.png')
+    phis_sol, comsol = run(time, utilities.picard_solver, return_comsol=True)
+    utilities.report(comsol.neg, time_in, phis_sol[:, comsol.neg_ind],
+                     comsol.data.phis[:, comsol.neg_ind][1::2], '$\Phi_s^{neg}$')
+    utilities.save_plot(__file__, 'plots/compare_phis_neg.png')
     plt.show()
-    utilities.report(comsol.pos, time, fenics[:, comsol.pos_ind], comsol.data.phis[:, comsol.pos_ind], '$\Phi_s^{neg}$')
-    plt.savefig('comsol_compare_phis_pos.png')
+    utilities.report(comsol.pos, time_in, phis_sol[:, comsol.pos_ind],
+                     comsol.data.phis[:, comsol.pos_ind][1::2], '$\Phi_s^{pos}$')
+    utilities.save_plot(__file__, 'plots/compare_phis_pos.png')
     plt.show()
 
 
