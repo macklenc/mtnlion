dbd0e9b235c0b4e4fe9041378f1dad5ea412e55b
Christopher Macklen <cmacklen@uccs.edu>
2018-10-23 13:11:59 -0400

    Got pseudo cse -> main cse interpolation working
    Many much refactoring desperately needed

--- buildup/fenics_/phase1/cs.py
+++ buildup/fenics_/phase1/cs.py
@@ -16,6 +16,7 @@ public:
   void eval(Array<double>& values, const Array<double>& x,
             const ufc::cell& c) const
   {      
+        std::cout << "input x: " << x << std::endl;
       switch((*markers)[c.index]){
         case 0:
             neg->eval(values, x, c);
@@ -36,6 +37,7 @@ public:
 };
 '''
 
+
 composition = '''
 class Composition : public Expression
 {
@@ -45,15 +47,30 @@ public:
   void eval(Array<double>& values, const Array<double>& x,
             const ufc::cell& c) const
   {
-      Array<double> val(x.size());
+      Array<double> val(2);
       inner->eval(val, x, c);
+      std::cout << val << std::endl;
       outer->eval(values, val, c);      
   }
   
   std::shared_ptr<GenericFunction> outer;
   std::shared_ptr<GenericFunction> inner;
+  unsigned num_dims;
 };
 '''
+
+
+def find_cse_from_cs(comsol):
+    data = np.append(comsol.pseudo_mesh, comsol.data.cs[1::2].T, axis=1)  # grab cs for each time
+    indices = np.where(np.abs(data[:, 1] - 1.0) <= 1e-5)[0]  # find the indices of the solution where r=1 (cse)
+    data = data[indices]  # reduce data set to only show cse
+    data = data[data[:, 0].argsort()]  # organize the coordinates for monotonicity
+    xcoor = data[:, 0]  # x coordinates are in the first column, y should always be 1 now
+    neg_ind = np.where(xcoor <= 1)[0]  # using the pseudo dims definition of neg and pos electrodes
+    pos_ind = np.where(xcoor >= 1.5)[0]
+    cse = data[:, 2:]  # first two columns are the coordinates
+
+    return xcoor, cse, neg_ind, pos_ind
 
 
 def run(time, dt, return_comsol=False):
@@ -62,6 +79,29 @@ def run(time, dt, return_comsol=False):
     pseudo_domain = cmn.pseudo_domain
     cse_domain = cmn.pseudo_cse_domain
 
+    boundaries = np.arange(4)
+    # Setup subdomain markers
+    neg_domain = fem.CompiledSubDomain('(x[0] >= (b1 - DOLFIN_EPS)) && (x[0] <= (b2 + DOLFIN_EPS))',
+                                       b1=boundaries[0].astype(np.double), b2=boundaries[1].astype(np.double))
+    sep_domain = fem.CompiledSubDomain('(x[0] >= b1 - DOLFIN_EPS) && (x[0] <= b2 + DOLFIN_EPS)',
+                                       b1=boundaries[1].astype(np.double), b2=boundaries[2].astype(np.double))
+    pos_domain = fem.CompiledSubDomain('(x[0] >= b1 - DOLFIN_EPS) && (x[0] <= b2 + DOLFIN_EPS)',
+                                       b1=boundaries[2].astype(np.double), b2=boundaries[3].astype(np.double))
+
+    combined_subdomains = fem.MeshFunction('size_t', domain.mesh, domain.mesh.topology().dim())
+    combined_subdomains.array()[domain.domain_markers.array() == 0] = 1
+    combined_subdomains.array()[domain.domain_markers.array() == 2] = 1
+    submesh = fem.SubMesh(domain.mesh, combined_subdomains, 1)
+    submesh_domain_markers = fem.MeshFunction('size_t', submesh, submesh.topology().dim())
+    submesh_domain_markers.set_all(99)
+    neg_domain.mark(submesh_domain_markers, 0)
+    sep_domain.mark(submesh_domain_markers, 1)
+    pos_domain.mark(submesh_domain_markers, 2)
+    jV = fem.FunctionSpace(submesh, 'Lagrange', 1)
+
+    # fem.plot(submesh)
+    # plt.show()
+
     cs_sol = utilities.create_solution_matrices(int(len(time) / 2), len(comsol.pseudo_mesh), 1)[0]
     cse_sol = utilities.create_solution_matrices(int(len(time) / 2), len(cse_domain.mesh.coordinates()[:, 0]), 1)[0]
 
@@ -69,31 +109,40 @@ def run(time, dt, return_comsol=False):
     v = fem.TestFunction(pseudo_domain.V)
 
     cs_1, cs, jbar2_interp = utilities.create_functions(pseudo_domain.V, 3)
-    jbar_c = utilities.create_functions(domain.V, 1)[0]
-    jbar2 = utilities.create_functions(cse_domain.V, 1)[0]
+    jbar_c, cse_2 = utilities.create_functions(domain.V, 2)
+    cse_t = utilities.create_functions(jV, 1)[0]
+    jbar2, cse = utilities.create_functions(cse_domain.V, 2)
     jbar2.set_allow_extrapolation(True)
+    cse.set_allow_extrapolation(True)
 
-    main_from_pseudo = fem.Expression(cppcode=x_conv, markers=cse_domain.domain_markers, degree=1)
+    main_from_pseudo = fem.Expression(cppcode=x_conv, markers=cse_domain.domain_markers, num_dims=1, degree=1)
     main_from_pseudo.neg = fem.Expression('x[0]', degree=1)
     main_from_pseudo.sep = fem.Expression('2*x[0]-1', degree=1)
     main_from_pseudo.pos = fem.Expression('x[0] + 0.5', degree=1)
 
-    pseudo_from_main = fem.Expression(cppcode=x_conv, markers=domain.domain_markers, degree=1)
-    pseudo_from_main.neg = fem.Expression('x[0]', degree=1)
-    pseudo_from_main.sep = fem.Expression('0.5*(x[0]+1)', degree=1)
-    pseudo_from_main.pos = fem.Expression('x[0] + 0.5', degree=1)
+    pseudo_from_main = fem.Expression(cppcode=x_conv, markers=submesh_domain_markers, num_dims=2, degree=1)
+    pseudo_from_main.neg = fem.Expression(('x[0]', '1.0'), degree=1)
+    pseudo_from_main.sep = fem.Expression(('0.5*(x[0]+1)', '1.0'), degree=1)
+    pseudo_from_main.pos = fem.Expression(('x[0] - 0.5', '1.0'), degree=1)
 
-    # x = fem.Expression('x[0]', degree=1)
+    x = fem.Expression('x[0]', degree=1)
 
     # print(utilities.get_1d(fem.interpolate(main_x_from_pseudo, domain.V), domain.V))
+    # print(utilities.get_1d(fem.interpolate(x, X), X))
+
+    print(utilities.get_1d(fem.interpolate(pseudo_from_main, jV), jV))
     # print(utilities.get_1d(fem.interpolate(x, X), X))
     print(utilities.get_1d(fem.interpolate(main_from_pseudo, cse_domain.V), cse_domain.V))
 
     composition_ex = fem.Expression(cppcode=composition, inner=main_from_pseudo, outer=jbar_c, degree=1)
-    utilities.assign_functions([comsol.data.j], [jbar_c], domain.V, 4)
+    composition_ex_cse = fem.Expression(cppcode=composition, inner=pseudo_from_main, outer=cse, degree=1)
 
-    orig = utilities.get_1d(fem.interpolate(jbar_c, domain.V), domain.V)
-    test = utilities.get_1d(fem.interpolate(composition_ex, cse_domain.V), cse_domain.V)
+    # utilities.assign_functions([comsol.data.j], [jbar_c], domain.V, 4)
+    # orig = utilities.get_1d(fem.interpolate(jbar_c, domain.V), domain.V)
+    # test = utilities.get_1d(fem.interpolate(composition_ex, cse_domain.V), cse_domain.V)
+
+    # orig = utilities.get_1d(fem.interpolate(x, domain.V), domain.V)
+    # test = utilities.get_1d(fem.interpolate(composition_ex_cse, cse_domain.V), cse_domain.V)
     # print(test)
     # plt.plot(orig)
     # plt.show()
@@ -153,7 +202,16 @@ def run(time, dt, return_comsol=False):
         # Solve
         fem.solve(a == L, cs)
         cs.set_allow_extrapolation(True)
-        cse = fem.interpolate(cs, cse_domain.V)
+        cse.assign(fem.interpolate(cs, cse_domain.V))
+        cse_t.assign(fem.interpolate(composition_ex_cse, jV))
+
+        plt.plot(utilities.get_1d(cse_t, jV)[0:21])
+        plt.plot(comsol.data.cse[i, comsol.neg_ind])
+        plt.show()
+        plt.plot(utilities.get_1d(cse_t, jV)[21:])
+        plt.plot(comsol.data.cse[i, comsol.pos_ind])
+        plt.show()
+
         # asdf = utilities.get_1d(cse, X)
         # fdsa = np.concatenate(
         #     (asdf[:len(comsol_sol.neg)], np.zeros(len(comsol_sol.sep) - 2), asdf[len(comsol_sol.neg):]))
@@ -165,14 +223,7 @@ def run(time, dt, return_comsol=False):
 
     # exit()
     ## find c_se from c_s
-    data = np.append(comsol.pseudo_mesh, comsol.data.cs[1::2].T, axis=1)  # grab cs for each time
-    indices = np.where(np.abs(data[:, 1] - 1.0) <= 1e-5)[0]  # find the indices of the solution where r=1 (cse)
-    data = data[indices]  # reduce data set to only show cse
-    data = data[data[:, 0].argsort()]  # organize the coordinates for monotonicity
-    xcoor = data[:, 0]  # x coordinates are in the first column, y should always be 1 now
-    neg_ind = np.where(xcoor <= 1)[0]  # using the pseudo dims definition of neg and pos electrodes
-    pos_ind = np.where(xcoor >= 1.5)[0]
-    cse = data[:, 2:]  # first two columns are the coordinates
+    xcoor, cse, neg_ind, pos_ind = find_cse_from_cs(comsol)
 
     ## find c_se on the pseudo dim coordinates
     # tmpcse = np.concatenate((comsol_sol.data.cse[1::2, comsol_sol.neg_ind], comsol_sol.data.cse[1::2, comsol_sol.pos_ind]), axis=1)
