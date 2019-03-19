45ecd06ee2625acdedff1d803220b58ccabc58c7
Christopher Macklen <cmacklen@uccs.edu>
2019-03-19 19:41:04 +0000

    Added a more advanced tox config and fixed errors

--- mtnlion/domain.py
+++ mtnlion/domain.py
@@ -1,6 +1,4 @@
-"""
-Domain creation/manipulation.
-"""
+"""Domain creation/manipulation."""
 import logging
 from typing import List, Tuple, Union
 
@@ -13,7 +11,9 @@ logger = logging.getLogger(__name__)
 
 def subdomain(comparison: np.ndarray) -> slice:
     """
-    Find the indices of the requested subdomain, correcting for internal boundaries. I.e. if the mesh is defined by
+    Find the indices of the requested subdomain, correcting for internal boundaries.
+
+    I.e. if the mesh is defined by
     ``numpy.arange(0, 3, 0.1)`` and you wish to find the subdomain ``0 <= x <= 1`` then you would call::
         subdomain(mesh, x < 1)
     Subdomain returns ``x <= 1``, the reason for the exclusive less-than is to work around having repeated internal
@@ -39,8 +39,9 @@ def subdomain(comparison: np.ndarray) -> slice:
 
 def subdomains(mesh: np.ndarray, regions: List[Tuple[float, float]]):
     """
-    Find indices of given subdomains. For example separating a domain from [0, 3] into [0, 1], [1, 2], and [2, 3] would
-    be::
+    Find indices of given subdomains.
+
+     For example separating a domain from [0, 3] into [0, 1], [1, 2], and [2, 3] would be::
         subdomains(np.arange(0, 3, 0.1), [(0, 1), (1, 2), (2, 3)])
     :param mesh: one-dimensional list of domain values
     :param regions: two dimensional list containing multiple ranges for subdomains
@@ -52,7 +53,9 @@ def subdomains(mesh: np.ndarray, regions: List[Tuple[float, float]]):
 
 class ReferenceCell(engine.Mountain):
     """
-    Reference lithium-ion cell geometry, where the dimensions are normalized. The x dimension is defined such that the
+    Reference lithium-ion cell geometry, where the dimensions are normalized.
+
+    The x dimension is defined such that the
     negative electrode exists between [0, 1], the separator exists between [1, 2], and the positive electrode exists
     between [2, 3]. For convenience the subdomains are added onto engine.Mountain.
     """
@@ -60,7 +63,7 @@ class ReferenceCell(engine.Mountain):
     def __init__(self, mesh: np.ndarray, pseudo_mesh: np.ndarray, time_mesh: np.ndarray,
                  boundaries: Union[np.ndarray, List[float]], **kwargs: np.ndarray) -> None:
         """
-        Store the solutions to each cell parameter
+        Store the solutions to each cell parameter.
 
         :param mesh: Solution mesh
         :param boundaries: internal boundaries in the mesh
@@ -76,16 +79,16 @@ class ReferenceCell(engine.Mountain):
 
     def get_solution_in(self, subspace: str) -> Union[None, 'ReferenceCell']:
         """
-        Return the solution for only the given subspace
+        Return the solution for only the given subspace.
 
         :return: reduced solution set to only contain the given space
         """
         logging.debug('Retrieving solution in {}'.format(subspace))
-        if subspace is 'neg':
+        if subspace == 'neg':
             space = self.neg_ind
-        elif subspace is 'sep':
+        elif subspace == 'sep':
             space = self.sep_ind
-        elif subspace is 'pos':
+        elif subspace == 'pos':
             space = self.pos_ind
         else:
             return None
