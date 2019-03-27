8a0d698101afcbcdffb4bdd361c0e0be1875ca47
Christopher Macklen <cmacklen@uccs.edu>
2019-03-27 22:38:29 +0000

    Started the transition to FEniCS 2018.1.0.r1

--- mtnlion/headers/xbar.h
+++ mtnlion/headers/xbar.h
@@ -1,24 +1,42 @@
-class XBar : public Expression
-{
-public:
-	XBar() : Expression() {}
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+namespace py = pybind11;
 
-	void eval(Array<double>& values, const Array<double>& x, const ufc::cell& c) const {
-		switch((*markers)[c.index]){
-			case 0:
-				neg->eval(values, x, c);
-				break;
-			case 1:
-				sep->eval(values, x, c);
-				break;
-			case 2:
-				pos->eval(values, x, c);
-				break;
-		}
-	}
+#include <dolfin/function/Expression.h>
+#include <dolfin/function/GenericFunction.h>
+#include <dolfin/mesh/MeshFunction.h>
 
-	std::shared_ptr<MeshFunction<std::size_t>> markers;
-	std::shared_ptr<GenericFunction> neg;
-	std::shared_ptr<GenericFunction> sep;
-	std::shared_ptr<GenericFunction> pos;
+class XBar : public dolfin::Expression {
+ public:
+  XBar() : dolfin::Expression() {}
+
+  void eval(Eigen::Ref<Eigen::VectorXd> values,
+            Eigen::Ref<const Eigen::VectorXd> x, const ufc::cell& cell) const {
+    switch ((*markers)[cell.index]) {
+      case 0:
+        neg->eval(values, x);
+        break;
+      case 1:
+        sep->eval(values, x);
+        break;
+      case 2:
+        pos->eval(values, x);
+        break;
+    }
+  }
+
+  std::shared_ptr<dolfin::MeshFunction<std::size_t>> markers;
+  std::shared_ptr<dolfin::GenericFunction> neg;
+  std::shared_ptr<dolfin::GenericFunction> sep;
+  std::shared_ptr<dolfin::GenericFunction> pos;
 };
+
+PYBIND11_MODULE(SIGNATURE, m) {
+    py::class_<XBar, std::shared_ptr<XBar>, dolfin::Expression>
+    (m, "XBar")
+    .def(py::init<>())
+    .def_readwrite("markers", &XBar::markers)
+    .def_readwrite("neg", &XBar::neg)
+    .def_readwrite("sep", &XBar::sep)
+    .def_readwrite("pos", &XBar::pos);
+}
