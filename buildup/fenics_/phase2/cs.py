0db6ea4ba6376f11e10355b349ba6ccce5a02a98
Christopher Macklen <cmacklen@uccs.edu>
2019-03-28 05:49:20 +0000

    Ran the 'black' code formatter on the project

--- buildup/fenics_/phase2/cs.py
+++ buildup/fenics_/phase2/cs.py
@@ -11,19 +11,22 @@ def cross_domain(func, dest_markers, dest_x_neg, dest_x_sep, dest_x_pos, func_ce
     # NOTE: .cpp_object() will not be required later as per
     # https://bitbucket.org/fenics-project/dolfin/issues/1041/compiledexpression-cant-be-initialized
     # TODO: Use python wrappers
-    xbar = fem.CompiledExpression(fem.compile_cpp_code(utilities.expressions.xbar).XBar(),
-                                  markers=dest_markers,
-                                  neg=dest_x_neg.cpp_object(),
-                                  sep=dest_x_sep.cpp_object(),
-                                  pos=dest_x_pos.cpp_object(),
-                                  degree=1,
+    xbar = fem.CompiledExpression(
+        fem.compile_cpp_code(utilities.expressions.xbar).XBar(),
+        markers=dest_markers,
+        neg=dest_x_neg.cpp_object(),
+        sep=dest_x_sep.cpp_object(),
+        pos=dest_x_pos.cpp_object(),
+        degree=1,
     )
-    return fem.CompiledExpression(fem.compile_cpp_code(utilities.expressions.composition).Composition(),
-                                  inner=xbar.cpp_object(),
-                                  outer=func.cpp_object(),
-                                  inner_cell=dest_cell,
-                                  outer_cell=func_cell,
-                                  degree=1)
+    return fem.CompiledExpression(
+        fem.compile_cpp_code(utilities.expressions.composition).Composition(),
+        inner=xbar.cpp_object(),
+        outer=func.cpp_object(),
+        inner_cell=dest_cell,
+        outer_cell=func_cell,
+        degree=1,
+    )
 
 
 def run(time, dt, return_comsol=False):
