0e1dd27ea666a1cc7a45f6672a6332efaf19da0d
Christopher Macklen <cmacklen@uccs.edu>
2019-03-27 23:44:59 -0600

    Added multiple dockerfile options

--- launch_sde.sh
+++ launch_sde.sh
@@ -5,4 +5,4 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 chmod 644 $XAUTH
-docker run -ti --rm -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -e DISPLAY=$DISPLAY -v sde_vol:/home/mtnlion macklenc/sde:latest
+docker run -ti --rm --privileged -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -e DISPLAY=$DISPLAY -v sde_vol:/home/mtnlion macklenc/sde:$1
