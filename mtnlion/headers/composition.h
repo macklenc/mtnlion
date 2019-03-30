61019b053d7901266bd6299c091bb7a9800fbd42
Christopher Macklen <cmacklen@uccs.edu>
2019-03-30 01:02:31 +0000

    Revert "Updated headers to use the Eigen library."
    This reverts commit c59496b24c0ca83adaf21bb666f3e493f0c00558.

--- mtnlion/headers/composition.h
+++ mtnlion/headers/composition.h
@@ -1,14 +1,15 @@
-class Composition : public Expression {
- public:
-  Composition() : Expression() {}
+class Composition : public Expression
+{
+public:
+    Composition() : Expression() {}
 
-  void eval(Eigen::Ref<Eigen::VectorXd>& values,
-            Eigen::Ref<const Eigen::VectorXd>& x, const ufc::cell& c) const {
-    Array<double> val(3);
-    inner->eval(val, x, c);
-    outer->eval(values, val, c);
-  }
+    void eval(Array<double>& values, const Array<double>& x, const ufc::cell& c) const
+    {
+        Array<double> val(3);
+        inner->eval(val, x, c);
+        outer->eval(values, val, c);
+    }
 
-  std::shared_ptr<GenericFunction> outer;
-  std::shared_ptr<GenericFunction> inner;
+    std::shared_ptr<GenericFunction> outer;
+    std::shared_ptr<GenericFunction> inner;
 };
