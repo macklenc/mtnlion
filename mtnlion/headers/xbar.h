b986b5a8822ca3720779f6d786e94cfb76a89c0d
Christopher Macklen <cmacklen@uccs.edu>
2019-03-30 01:02:31 +0000

    Revert "Updated headers to use the Eigen library."
    This reverts commit c59496b24c0ca83adaf21bb666f3e493f0c00558.

--- mtnlion/headers/xbar.h
+++ mtnlion/headers/xbar.h
@@ -1,24 +1,24 @@
-class XBar : public Expression {
- public:
-  XBar() : Expression() {}
+class XBar : public Expression
+{
+public:
+	XBar() : Expression() {}
 
-  void eval(Eigen::Ref<Eigen::VectorXd>& values,
-            Eigen::Ref<const Eigen::VectorXd>& x, const ufc::cell& c) const {
-    switch ((*markers)[c.index]) {
-      case 0:
-        neg->eval(values, x, c);
-        break;
-      case 1:
-        sep->eval(values, x, c);
-        break;
-      case 2:
-        pos->eval(values, x, c);
-        break;
-    }
-  }
+	void eval(Array<double>& values, const Array<double>& x, const ufc::cell& c) const {
+		switch((*markers)[c.index]){
+			case 0:
+				neg->eval(values, x, c);
+				break;
+			case 1:
+				sep->eval(values, x, c);
+				break;
+			case 2:
+				pos->eval(values, x, c);
+				break;
+		}
+	}
 
-  std::shared_ptr<MeshFunction<std::size_t>> markers;
-  std::shared_ptr<GenericFunction> neg;
-  std::shared_ptr<GenericFunction> sep;
-  std::shared_ptr<GenericFunction> pos;
+	std::shared_ptr<MeshFunction<std::size_t>> markers;
+	std::shared_ptr<GenericFunction> neg;
+	std::shared_ptr<GenericFunction> sep;
+	std::shared_ptr<GenericFunction> pos;
 };
