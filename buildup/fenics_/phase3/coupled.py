50315096d2e6b745d41cb571a33a49dd4246ce7d
Christopher Macklen <cmacklen@uccs.edu>
2019-03-30 20:09:27 +0000

    Updated phase3 to work with FEniCS 2018

--- buildup/fenics_/phase3/coupled.py
+++ buildup/fenics_/phase3/coupled.py
@@ -1,8 +1,10 @@
-import fenics as fem
+import dolfin as fem
 import matplotlib.pyplot as plt
 
-from buildup import (common, utilities)
+from buildup import common, utilities
 from mtnlion.newman import equations
+
+# DOESN'T WORK
 
 
 def run(time, dt, return_comsol=False):
@@ -14,12 +16,12 @@ def run(time, dt, return_comsol=False):
     comsol_ce = utilities.interp_time(comsol.time_mesh, comsol.data.ce)
     comsol_cse = utilities.interp_time(comsol.time_mesh, comsol.data.cse)
 
-    pseudo_domain = cmn.pseudo_domain
+    # pseudo_domain = cmn.pseudo_domain
     electrode_domain = cmn.electrode_domain
 
-    phis_elem = fem.FiniteElement('CG', electrode_domain.mesh.ufl_cell(), 1)
-    phie_elem = fem.FiniteElement('CG', domain.mesh.ufl_cell(), 1)
-    ce_elem = fem.FiniteElement('CG', domain.mesh.ufl_cell(), 1)
+    phis_elem = fem.FiniteElement("CG", electrode_domain.mesh.ufl_cell(), 1)
+    phie_elem = fem.FiniteElement("CG", domain.mesh.ufl_cell(), 1)
+    ce_elem = fem.FiniteElement("CG", domain.mesh.ufl_cell(), 1)
     # cs_elem = fem.FiniteElement('CG', pseudo_domain.mesh.ufl_cell(), 1)
 
     W_elem = fem.MixedElement([phis_elem, phie_elem, ce_elem])
@@ -44,24 +46,28 @@ def run(time, dt, return_comsol=False):
     Lc = cmn.fenics_params.L
     de_eff = cmn.fenics_params.De_eff
     phis_neumann = Iapp / cmn.fenics_consts.Acell * v_phis * electrode_domain.ds(4)
-    phie_newmann_a = (kappa_eff('-') / Lc('-') * fem.inner(fem.grad(phie('-')), domain.n('-')) * v_phie('-') +
-                      kappa_eff('+') / Lc('+') * fem.inner(fem.grad(phie('+')), domain.n('+')) * v_phie('+')) * \
-                     (domain.dS(2) + domain.dS(3))
+    phie_newmann_a = (
+        kappa_eff("-") / Lc("-") * fem.inner(fem.grad(phie("-")), domain.n("-")) * v_phie("-")
+        + kappa_eff("+") / Lc("+") * fem.inner(fem.grad(phie("+")), domain.n("+")) * v_phie("+")
+    ) * (domain.dS(2) + domain.dS(3))
 
-    phie_newmann_L = -(kappa_Deff('-') / Lc('-') * fem.inner(fem.grad(fem.ln(ce_c('-'))), domain.n('-')) * v_phie('-') +
-                       kappa_Deff('+') / Lc('+') * fem.inner(fem.grad(fem.ln(ce_c('+'))), domain.n('+')) * v_phie(
-            '+')) * \
-                     (domain.dS(2) + domain.dS(3))
+    phie_newmann_L = -(
+        kappa_Deff("-") / Lc("-") * fem.inner(fem.grad(fem.ln(ce_c("-"))), domain.n("-")) * v_phie("-")
+        + kappa_Deff("+") / Lc("+") * fem.inner(fem.grad(fem.ln(ce_c("+"))), domain.n("+")) * v_phie("+")
+    ) * (domain.dS(2) + domain.dS(3))
 
-    ce_neumann = de_eff('-') / Lc('-') * fem.inner(fem.grad(ce('-')), domain.n('-')) * v_ce('-') * domain.dS(2) + \
-                 de_eff('+') / Lc('+') * fem.inner(fem.grad(ce('+')), domain.n('+')) * v_ce('+') * domain.dS(2) + \
-                 de_eff('-') / Lc('-') * fem.inner(fem.grad(ce('-')), domain.n('-')) * v_ce('-') * domain.dS(3) + \
-                 de_eff('+') / Lc('+') * fem.inner(fem.grad(ce('+')), domain.n('+')) * v_ce('+') * domain.dS(3)
+    ce_neumann = (
+        de_eff("-") / Lc("-") * fem.inner(fem.grad(ce("-")), domain.n("-")) * v_ce("-") * domain.dS(2)
+        + de_eff("+") / Lc("+") * fem.inner(fem.grad(ce("+")), domain.n("+")) * v_ce("+") * domain.dS(2)
+        + de_eff("-") / Lc("-") * fem.inner(fem.grad(ce("-")), domain.n("-")) * v_ce("-") * domain.dS(3)
+        + de_eff("+") / Lc("+") * fem.inner(fem.grad(ce("+")), domain.n("+")) * v_ce("+") * domain.dS(3)
+    )
 
     euler = equations.euler(ce, ce_1, dtc)
     phis_lhs, phis_rhs = equations.phis(j, phis, v_phis, **cmn.fenics_params, **cmn.fenics_consts)
-    phie_lhs, phie_rhs1, phie_rhs2 = equations.phie(j, ce_c, phie, v_phie, kappa_eff, kappa_Deff,
-                                                    **cmn.fenics_params, **cmn.fenics_consts)
+    phie_lhs, phie_rhs1, phie_rhs2 = equations.phie(
+        j, ce_c, phie, v_phie, kappa_eff, kappa_Deff, **cmn.fenics_params, **cmn.fenics_consts
+    )
     ce_lhs, ce_rhs1, ce_rhs2 = equations.ce(j, ce, v_ce, **cmn.fenics_params, **cmn.fenics_consts)
 
     F = (phis_lhs - phis_rhs) * domain.dx((0, 2)) + fem.dot(phis, v_phis) * domain.dx(1) - phis_neumann
@@ -69,8 +75,12 @@ def run(time, dt, return_comsol=False):
     F += (ce_lhs * euler - ce_rhs1) * domain.dx - ce_rhs2 * domain.dx((0, 2)) + ce_neumann
 
     for k, t in enumerate(time):
-        utilities.assign_functions([comsol_phis(t), comsol_phie(t), comsol_ce(t), comsol_cse(t)],
-                                   [phis_c_, phie_c, ce_c, cse_c], domain.V, ...)
+        utilities.assign_functions(
+            [comsol_phis(t), comsol_phie(t), comsol_ce(t), comsol_cse(t)],
+            [phis_c_, phie_c, ce_c, cse_c],
+            domain.V,
+            Ellipsis,
+        )
         # utilities.assign_functions([comsol_ce(t - dt)],
         #                            [ce_1], domain.V, ...)
         Iapp.assign(float(cmn.Iapp(t)))
@@ -82,10 +92,10 @@ def run(time, dt, return_comsol=False):
         solver = fem.NonlinearVariationalSolver(problem)
 
         prm = solver.parameters
-        prm['newton_solver']['absolute_tolerance'] = 1e-8
-        prm['newton_solver']['relative_tolerance'] = 1e-7
-        prm['newton_solver']['maximum_iterations'] = 100
-        prm['newton_solver']['relaxation_parameter'] = 1.0
+        prm["newton_solver"]["absolute_tolerance"] = 1e-8
+        prm["newton_solver"]["relative_tolerance"] = 1e-7
+        prm["newton_solver"]["maximum_iterations"] = 100
+        prm["newton_solver"]["relaxation_parameter"] = 1.0
         solver.solve()
 
         ce_1.assign(u.sub(2))
@@ -99,7 +109,7 @@ def run(time, dt, return_comsol=False):
 
 
 def main():
-    fem.set_log_level(fem.INFO)
+    fem.set_log_level(fem.LogLevel.INFO)
 
     # Times at which to run solver
     time = [0.1, 5, 10, 15, 20]
@@ -109,15 +119,25 @@ def main():
     phis_sol, comsol = run(time, sim_dt, return_comsol=True)
     comsol_phis = utilities.interp_time(comsol.time_mesh, comsol.data.phis)
 
-    utilities.report(comsol.neg, time, phis_sol(plot_time)[:, comsol.neg_ind],
-                     comsol_phis(plot_time)[:, comsol.neg_ind], '$\Phi_s^{neg}$')
-    utilities.save_plot(__file__, 'plots/compare_phis_neg_newton.png')
+    utilities.report(
+        comsol.neg,
+        time,
+        phis_sol(plot_time)[:, comsol.neg_ind],
+        comsol_phis(plot_time)[:, comsol.neg_ind],
+        "$\Phi_s^{neg}$",
+    )
+    utilities.save_plot(__file__, "plots/compare_phis_neg_newton.png")
     plt.show()
-    utilities.report(comsol.pos, time, phis_sol(plot_time)[:, comsol.pos_ind],
-                     comsol_phis(plot_time)[:, comsol.pos_ind], '$\Phi_s^{pos}$')
-    utilities.save_plot(__file__, 'plots/compare_phis_pos_newton.png')
+    utilities.report(
+        comsol.pos,
+        time,
+        phis_sol(plot_time)[:, comsol.pos_ind],
+        comsol_phis(plot_time)[:, comsol.pos_ind],
+        "$\Phi_s^{pos}$",
+    )
+    utilities.save_plot(__file__, "plots/compare_phis_pos_newton.png")
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- buildup/fenics_/phase3/phie_phis_j.py
+++ buildup/fenics_/phase3/phie_phis_j.py
@@ -1,7 +1,7 @@
-import fenics as fem
+import dolfin as fem
 import matplotlib.pyplot as plt
 
-from buildup import (common, utilities)
+from buildup import common, utilities
 from mtnlion.newman import equations
 
 
@@ -14,8 +14,8 @@ def run(time, dt, return_comsol=False):
     comsol_cse = utilities.interp_time(comsol.time_mesh, comsol.data.cse)
     comsol_j = utilities.interp_time(comsol.time_mesh, comsol.data.j)
 
-    phis_elem = fem.FiniteElement('CG', domain.mesh.ufl_cell(), 1)
-    phie_elem = fem.FiniteElement('CG', domain.mesh.ufl_cell(), 1)
+    phis_elem = fem.FiniteElement("CG", domain.mesh.ufl_cell(), 1)
+    phie_elem = fem.FiniteElement("CG", domain.mesh.ufl_cell(), 1)
 
     W_elem = fem.MixedElement([phis_elem, phie_elem])
     W = fem.FunctionSpace(domain.mesh, W_elem)
@@ -34,8 +34,9 @@ def run(time, dt, return_comsol=False):
     Iapp = fem.Constant(0)
 
     # Uocp = equations.Uocp(cse_c, **cmn.fenics_params)
-    Uocp = equations.Uocp_interp(cmn.Uocp_spline.Uocp_neg, cmn.Uocp_spline.Uocp_pos,
-                                 cse_c, cmn.fenics_params.csmax, utilities)
+    Uocp = equations.Uocp_interp(
+        cmn.Uocp_spline.Uocp_neg, cmn.Uocp_spline.Uocp_pos, cse_c, cmn.fenics_params.csmax, utilities
+    )
     j = equations.j(ce_c, cse_c, u.sub(1), u.sub(0), Uocp, **cmn.fenics_params, **cmn.fenics_consts)
     kappa_eff, kappa_Deff = common.kappa_Deff(ce_c, **cmn.fenics_params, **cmn.fenics_consts)
     # j = j_c
@@ -43,18 +44,20 @@ def run(time, dt, return_comsol=False):
     Lc = cmn.fenics_params.L
 
     phis_neumann = Iapp / cmn.fenics_consts.Acell * v_phis * domain.ds(4)
-    phie_newmann_a = (kappa_eff('-') / Lc('-') * fem.inner(fem.grad(phie('-')), domain.n('-')) * v_phie('-') +
-                      kappa_eff('+') / Lc('+') * fem.inner(fem.grad(phie('+')), domain.n('+')) * v_phie('+')) * \
-                     (domain.dS(2) + domain.dS(3))
+    phie_newmann_a = (
+        kappa_eff("-") / Lc("-") * fem.inner(fem.grad(phie("-")), domain.n("-")) * v_phie("-")
+        + kappa_eff("+") / Lc("+") * fem.inner(fem.grad(phie("+")), domain.n("+")) * v_phie("+")
+    ) * (domain.dS(2) + domain.dS(3))
 
-    phie_newmann_L = -(kappa_Deff('-') / Lc('-') * fem.inner(fem.grad(fem.ln(ce_c('-'))), domain.n('-')) * v_phie('-') +
-                       kappa_Deff('+') / Lc('+') * fem.inner(fem.grad(fem.ln(ce_c('+'))), domain.n('+')) * v_phie(
-            '+')) * \
-                     (domain.dS(2) + domain.dS(3))
+    phie_newmann_L = -(
+        kappa_Deff("-") / Lc("-") * fem.inner(fem.grad(fem.ln(ce_c("-"))), domain.n("-")) * v_phie("-")
+        + kappa_Deff("+") / Lc("+") * fem.inner(fem.grad(fem.ln(ce_c("+"))), domain.n("+")) * v_phie("+")
+    ) * (domain.dS(2) + domain.dS(3))
 
     phis_lhs, phis_rhs = equations.phis(j, phis, v_phis, **cmn.fenics_params, **cmn.fenics_consts)
-    phie_lhs, phie_rhs1, phie_rhs2 = equations.phie(j, ce_c, phie, v_phie, kappa_eff, kappa_Deff,
-                                                    **cmn.fenics_params, **cmn.fenics_consts)
+    phie_lhs, phie_rhs1, phie_rhs2 = equations.phie(
+        j, ce_c, phie, v_phie, kappa_eff, kappa_Deff, **cmn.fenics_params, **cmn.fenics_consts
+    )
 
     F = (phis_lhs - phis_rhs) * domain.dx((0, 2)) + fem.dot(phis, v_phis) * domain.dx(1) - phis_neumann
     F += (phie_lhs - phie_rhs1) * domain.dx - phie_rhs2 * domain.dx((0, 2)) + phie_newmann_a - phie_newmann_L
@@ -68,8 +71,7 @@ def run(time, dt, return_comsol=False):
 
     for k, t in enumerate(time):
         utilities.assign_functions([comsol_phis(t - dt), comsol_phie(t - dt)], [phis_c_, phie_c_], domain.V, ...)
-        utilities.assign_functions([comsol_ce(t), comsol_cse(t), comsol_j(t)],
-                                   [ce_c, cse_c, j_c], domain.V, ...)
+        utilities.assign_functions([comsol_ce(t), comsol_cse(t), comsol_j(t)], [ce_c, cse_c, j_c], domain.V, ...)
         fem.assign(u.sub(0), phis_c_)
         fem.assign(u.sub(1), phie_c_)
         Iapp.assign(float(cmn.Iapp(t)))
@@ -80,10 +82,10 @@ def run(time, dt, return_comsol=False):
         solver = fem.NonlinearVariationalSolver(problem)
 
         prm = solver.parameters
-        prm['newton_solver']['absolute_tolerance'] = 1e-8
-        prm['newton_solver']['relative_tolerance'] = 1e-8
-        prm['newton_solver']['maximum_iterations'] = 1000
-        prm['newton_solver']['relaxation_parameter'] = 0.18
+        prm["newton_solver"]["absolute_tolerance"] = 1e-8
+        prm["newton_solver"]["relative_tolerance"] = 1e-8
+        prm["newton_solver"]["maximum_iterations"] = 1000
+        prm["newton_solver"]["relaxation_parameter"] = 0.18
         solver.solve()
 
         fem.solve(a_j == L_j, j_c)
@@ -97,15 +99,22 @@ def run(time, dt, return_comsol=False):
         j_sol[k, :] = utilities.get_1d(fem.interpolate(j_c, domain.V), domain.V)
 
     if return_comsol:
-        return utilities.interp_time(time, phis_sol), utilities.interp_time(time, phie_sol), \
-               utilities.interp_time(time, j_sol), comsol
+        return (
+            utilities.interp_time(time, phis_sol),
+            utilities.interp_time(time, phie_sol),
+            utilities.interp_time(time, j_sol),
+            comsol,
+        )
     else:
-        return utilities.interp_time(time, phis_sol), utilities.interp_time(time, phie_sol), \
-               utilities.interp_time(time, j_sol)
+        return (
+            utilities.interp_time(time, phis_sol),
+            utilities.interp_time(time, phie_sol),
+            utilities.interp_time(time, j_sol),
+        )
 
 
 def main():
-    fem.set_log_level(fem.INFO)
+    fem.set_log_level(fem.LogLevel.INFO)
 
     # Times at which to run solver
     time = [1, 5, 10, 15, 20]
@@ -117,28 +126,40 @@ def main():
     comsol_phie = utilities.interp_time(comsol.time_mesh, comsol.data.phie)
     comsol_j = utilities.interp_time(comsol.time_mesh, comsol.data.j)
 
-    utilities.report(comsol.neg, time, phis_sol(plot_time)[:, comsol.neg_ind],
-                     comsol_phis(plot_time)[:, comsol.neg_ind], '$\Phi_s^{neg}$')
-    utilities.save_plot(__file__, 'plots/coupled_phie_phis_j/compare_phis_neg_newton.png')
+    utilities.report(
+        comsol.neg,
+        time,
+        phis_sol(plot_time)[:, comsol.neg_ind],
+        comsol_phis(plot_time)[:, comsol.neg_ind],
+        "$\Phi_s^{neg}$",
+    )
+    utilities.save_plot(__file__, "plots/coupled_phie_phis_j/compare_phis_neg_newton.png")
     plt.show()
-    utilities.report(comsol.pos, time, phis_sol(plot_time)[:, comsol.pos_ind],
-                     comsol_phis(plot_time)[:, comsol.pos_ind], '$\Phi_s^{pos}$')
-    utilities.save_plot(__file__, 'plots/coupled_phie_phis_j/compare_phis_pos_newton.png')
-    plt.show()
-
-    utilities.report(comsol.mesh, time, phie_sol(plot_time), comsol_phie(plot_time), '$\Phi_e$')
-    utilities.save_plot(__file__, 'plots/coupled_phie_phis_j/compare_phie.png')
-    plt.show()
-
-    utilities.report(comsol.neg, time, j_sol(plot_time)[:, comsol.neg_ind],
-                     comsol_j(plot_time)[:, comsol.neg_ind], '$j^{neg}$')
-    utilities.save_plot(__file__, 'plots/coupled_phie_phis_j/compare_j_neg_newton.png')
-    plt.show()
-    utilities.report(comsol.pos, time, j_sol(plot_time)[:, comsol.pos_ind],
-                     comsol_j(plot_time)[:, comsol.pos_ind], '$j^{pos}$')
-    utilities.save_plot(__file__, 'plots/coupled_phie_phis_j/compare_j_pos_newton.png')
+    utilities.report(
+        comsol.pos,
+        time,
+        phis_sol(plot_time)[:, comsol.pos_ind],
+        comsol_phis(plot_time)[:, comsol.pos_ind],
+        "$\Phi_s^{pos}$",
+    )
+    utilities.save_plot(__file__, "plots/coupled_phie_phis_j/compare_phis_pos_newton.png")
     plt.show()
 
+    utilities.report(comsol.mesh, time, phie_sol(plot_time), comsol_phie(plot_time), "$\Phi_e$")
+    utilities.save_plot(__file__, "plots/coupled_phie_phis_j/compare_phie.png")
+    plt.show()
 
-if __name__ == '__main__':
+    utilities.report(
+        comsol.neg, time, j_sol(plot_time)[:, comsol.neg_ind], comsol_j(plot_time)[:, comsol.neg_ind], "$j^{neg}$"
+    )
+    utilities.save_plot(__file__, "plots/coupled_phie_phis_j/compare_j_neg_newton.png")
+    plt.show()
+    utilities.report(
+        comsol.pos, time, j_sol(plot_time)[:, comsol.pos_ind], comsol_j(plot_time)[:, comsol.pos_ind], "$j^{pos}$"
+    )
+    utilities.save_plot(__file__, "plots/coupled_phie_phis_j/compare_j_pos_newton.png")
+    plt.show()
+
+
+if __name__ == "__main__":
     main()
